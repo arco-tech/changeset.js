7673af8da4ce8c18828de2da94bc0a7abcaff6ea
insprac <mail@aydan.io>
2021-01-08 21:12:45 +1300

    Added additional functions, verifiers and tests

--- src/__tests__/changeset.test.ts
+++ src/__tests__/changeset.test.ts
@@ -1,15 +1,435 @@
 import { Changeset } from "../changeset"
 
-test("Constructs with given options", () => {
-  const changeset = new Changeset({
-    originals: { test: "original" },
-    changes: { test: "change" },
-    errors: { nonNull: [{ message: "Can't be blank" }] },
+describe("constructor", () => {
+  test("creates a valid changeset with given options", () => {
+    const changeset = new Changeset({
+      originals: { test: "original" },
+      changes: { test: "change" },
+      errors: { test: [{ message: "Error" }] },
+    })
+
+    expect(changeset.getOriginals()).toStrictEqual({ test: "original" })
+    expect(changeset.getChanges()).toStrictEqual({ test: "change" })
+    expect(changeset.getAllErrors()).toStrictEqual({
+      test: [{ message: "Error" }],
+    })
   })
 
-  expect(changeset.getOriginals()).toStrictEqual({ test: "original" })
-  expect(changeset.getChanges()).toStrictEqual({ test: "change" })
-  expect(changeset.getAllErrors()).toStrictEqual({
-    nonNull: [{ message: "Can't be blank" }],
+  test("throws error when originals is badly formatted", () => {
+    function cs(originals: any) {
+      return () => {
+        new Changeset({ originals })
+      }
+    }
+
+    expect(cs("bad")).toThrow("must be an object")
+    expect(cs(123)).toThrow("must be an object")
+    expect(cs(true)).toThrow("must be an object")
+    expect(cs([])).toThrow("can't be an array")
+  })
+
+  test("throws error when changes is badly formatted", () => {
+    function cs(changes: any) {
+      return () => {
+        new Changeset({ changes })
+      }
+    }
+
+    expect(cs("bad")).toThrow("must be an object")
+    expect(cs(123)).toThrow("must be an object")
+    expect(cs(true)).toThrow("must be an object")
+    expect(cs([])).toThrow("can't be an array")
+  })
+
+  test("throws error when errors is badly formatted", () => {
+    function cs(errors: any) {
+      return () => {
+        new Changeset({ errors })
+      }
+    }
+
+    expect(cs("bad")).toThrow("must be an object")
+    expect(cs(123)).toThrow("must be an object")
+    expect(cs(true)).toThrow("must be an object")
+    expect(cs([])).toThrow("can't be an array")
+    expect(cs({ x: "bad" })).toThrow("must be an array")
+    expect(cs({ x: 123 })).toThrow("must be an array")
+    expect(cs({ x: {} })).toThrow("must be an array")
+    expect(cs({ x: [{}] })).toThrow("must have a message property")
+    expect(cs({ x: [{ message: true }] })).toThrow("message must be a string")
+    expect(cs({ x: [{ message: 1 }] })).toThrow("message must be a string")
+  })
+})
+
+describe("getValue()", () => {
+  test("prioritises change over original", () => {
+    const changeset = new Changeset({
+      originals: { test: 12 },
+      changes: { test: 53 },
+    })
+
+    expect(changeset.getValue("test")).toEqual(53)
+    changeset.setChange("test", null)
+    expect(changeset.getValue("test")).toEqual(null)
+    changeset.setChange("test", undefined)
+    expect(changeset.getValue("test")).toEqual(undefined)
+  })
+
+  test("returns original when change isn't provided", () => {
+    const changeset = new Changeset({ originals: { test: 12 } })
+    expect(changeset.getValue("test")).toEqual(12)
+  })
+
+  test("returns undefined if neither change or original is provided", () => {
+    const changeset = new Changeset()
+    expect(changeset.getValue("test")).toEqual(undefined)
+  })
+})
+
+describe("getValues()", () => {
+  test("combines originals and changes, prioritising changes", () => {
+    const changeset = new Changeset({
+      originals: { original: "original", merged: "original" },
+      changes: { change: "change", merged: "change" },
+    })
+
+    expect(changeset.getValues()).toStrictEqual({
+      original: "original",
+      change: "change",
+      merged: "change",
+    })
+  })
+})
+
+describe("getOriginals()", () => {
+  test("returns a copy of originals", () => {
+    const changeset = new Changeset({ originals: { test: "yes" } })
+    const originals = changeset.getOriginals()
+    expect(originals).toStrictEqual({ test: "yes" })
+    originals.test = "updated"
+    expect(changeset.getOriginals()).toStrictEqual({ test: "yes" })
+  })
+})
+
+describe("getOriginal()", () => {
+  test("returns the original with the given field", () => {
+    const changeset = new Changeset({ originals: { test: 5 } })
+    expect(changeset.getOriginal("test")).toEqual(5)
+  })
+
+  test("returns undefined if the given field hasn't been set", () => {
+    const changeset = new Changeset()
+    expect(changeset.getOriginal("test")).toEqual(undefined)
+  })
+})
+
+describe("setOriginal()", () => {
+  test("updates the original field", () => {
+    const changeset = new Changeset({ originals: { test: "original" } })
+    changeset.setOriginal("test", "updated")
+    changeset.setOriginal("other", "new")
+
+    expect(changeset.getOriginals()).toStrictEqual({
+      test: "updated",
+      other: "new",
+    })
+  })
+})
+
+describe("setOriginals()", () => {
+  test("updates all originals, overriding any existing values", () => {
+    const changeset = new Changeset({
+      originals: {
+        test: "original",
+        other: "original",
+      },
+    })
+
+    changeset.setOriginals({ test: "updated" })
+    expect(changeset.getOriginals()).toStrictEqual({ test: "updated" })
+  })
+
+  test("throws an error when originals is an invalid type", () => {
+    const changeset = new Changeset()
+
+    function upd(originals: any) {
+      return () => {
+        changeset.setOriginals(originals)
+      }
+    }
+
+    expect(upd(null)).toThrow("can't be null")
+    expect(upd(undefined)).toThrow("must be an object")
+    expect(upd("bad")).toThrow("must be an object")
+    expect(upd(123)).toThrow("must be an object")
+    expect(upd(true)).toThrow("must be an object")
+    expect(upd([])).toThrow("can't be an array")
+  })
+})
+
+describe("hasOriginal()", () => {
+  test("true when provided original is set", () => {
+    const changeset = new Changeset({ originals: { test: undefined } })
+    expect(changeset.hasOriginal("test")).toEqual(true)
+  })
+
+  test("false when provided original is not set", () => {
+    const changeset = new Changeset()
+    expect(changeset.hasOriginal("test")).toEqual(false)
+  })
+})
+
+describe("getChanges()", () => {
+  test("returns a copy of changes", () => {
+    const changeset = new Changeset({ changes: { test: "yes" } })
+    const changes = changeset.getChanges()
+    expect(changes).toStrictEqual({ test: "yes" })
+    changes.test = "updated"
+    expect(changeset.getChanges()).toStrictEqual({ test: "yes" })
+  })
+})
+
+describe("getChange()", () => {
+  test("returns the change with the given field", () => {
+    const changeset = new Changeset({ changes: { test: 5 } })
+    expect(changeset.getChange("test")).toEqual(5)
+  })
+
+  test("returns undefined if the given field hasn't been set", () => {
+    const changeset = new Changeset()
+    expect(changeset.getChange("test")).toEqual(undefined)
+  })
+})
+
+describe("setChange()", () => {
+  test("updates the change field", () => {
+    const changeset = new Changeset({ changes: { test: "change" } })
+    changeset.setChange("test", "updated")
+    changeset.setChange("other", "new")
+
+    expect(changeset.getChanges()).toStrictEqual({
+      test: "updated",
+      other: "new",
+    })
+  })
+})
+
+describe("setChanges()", () => {
+  test("updates all changes, overriding any existing values", () => {
+    const changeset = new Changeset({
+      changes: {
+        test: "change",
+        other: "change",
+      },
+    })
+
+    changeset.setChanges({ test: "updated" })
+    expect(changeset.getChanges()).toStrictEqual({ test: "updated" })
+  })
+
+  test("throws an error when changes is an invalid type", () => {
+    const changeset = new Changeset()
+
+    function upd(changes: any) {
+      return () => {
+        changeset.setChanges(changes)
+      }
+    }
+
+    expect(upd(null)).toThrow("can't be null")
+    expect(upd(undefined)).toThrow("must be an object")
+    expect(upd("bad")).toThrow("must be an object")
+    expect(upd(123)).toThrow("must be an object")
+    expect(upd(true)).toThrow("must be an object")
+    expect(upd([])).toThrow("can't be an array")
+  })
+})
+
+describe("hasChange()", () => {
+  test("true when provided change is set", () => {
+    const changeset = new Changeset({ changes: { test: undefined } })
+    expect(changeset.hasChange("test")).toEqual(true)
+  })
+
+  test("false when provided change is not set", () => {
+    const changeset = new Changeset()
+    expect(changeset.hasChange("test")).toEqual(false)
+  })
+})
+
+describe("clearChanges()", () => {
+  test("removes all changes", () => {
+    const changeset = new Changeset({ changes: { a: 1, b: 2 } })
+    changeset.clearChanges()
+    expect(changeset.getChanges()).toEqual({})
+  })
+})
+
+describe("getAllErrors()", () => {
+  test("returns all errors", () => {
+    const changeset = new Changeset({
+      errors: {
+        test: [{ message: "Test Message" }],
+      },
+    })
+
+    expect(changeset.getAllErrors()).toStrictEqual({
+      test: [{ message: "Test Message" }],
+    })
+  })
+})
+
+describe("getErrors()", () => {
+  test("returns all field errors", () => {
+    const changeset = new Changeset({
+      errors: {
+        test: [{ message: "Error" }],
+      },
+    })
+
+    expect(changeset.getErrors("test")).toStrictEqual([{ message: "Error" }])
+  })
+
+  test("returns an empty array if field doesn't have any errors", () => {
+    const changeset = new Changeset()
+    expect(changeset.getErrors("test")).toStrictEqual([])
+  })
+})
+
+describe("setErrors()", () => {
+  test("sets a given field's errors", () => {
+    const changeset = new Changeset()
+    changeset.setErrors("test", [{ message: "Error" }])
+
+    expect(changeset.getAllErrors()).toStrictEqual({
+      test: [{ message: "Error" }],
+    })
+  })
+
+  test("throws an error when errors are badly formatted", () => {
+    const changeset = new Changeset()
+
+    function upd(errors: any) {
+      return () => {
+        changeset.setErrors("test", errors)
+      }
+    }
+
+    expect(upd("bad")).toThrow("must be an array")
+    expect(upd(123)).toThrow("must be an array")
+    expect(upd({})).toThrow("must be an array")
+    expect(upd([{}])).toThrow("must have a message property")
+    expect(upd([{ message: 123 }])).toThrow("message must be a string")
+  })
+})
+
+describe("setAllErrors()", () => {
+  test("sets all errors, overriding any existing errors", () => {
+    const changeset = new Changeset({
+      errors: {
+        existing: [{ message: "Error" }],
+      },
+    })
+
+    changeset.setAllErrors({
+      test: [{ message: "Error" }],
+    })
+
+    expect(changeset.getAllErrors()).toStrictEqual({
+      test: [{ message: "Error" }],
+    })
+  })
+
+  test("throws error when badly formatted", () => {
+    const changeset = new Changeset()
+
+    function upd(errors: any) {
+      return () => {
+        changeset.setAllErrors(errors)
+      }
+    }
+
+    expect(upd("bad")).toThrow("must be an object")
+    expect(upd(123)).toThrow("must be an object")
+    expect(upd(true)).toThrow("must be an object")
+    expect(upd([])).toThrow("can't be an array")
+    expect(upd({ x: "bad" })).toThrow("must be an array")
+    expect(upd({ x: 123 })).toThrow("must be an array")
+    expect(upd({ x: {} })).toThrow("must be an array")
+    expect(upd({ x: [{}] })).toThrow("must have a message property")
+    expect(upd({ x: [{ message: true }] })).toThrow("message must be a string")
+    expect(upd({ x: [{ message: 1 }] })).toThrow("message must be a string")
+  })
+})
+
+describe("hasErrors()", () => {
+  test("true when errors have been set", () => {
+    const changeset = new Changeset({
+      errors: {
+        test: [{ message: "Error" }],
+      },
+    })
+
+    expect(changeset.hasErrors("test")).toEqual(true)
+  })
+
+  test("false when errors haven't been set or errors are empty", () => {
+    const changeset = new Changeset({
+      errors: {
+        empty: [],
+      },
+    })
+
+    expect(changeset.hasErrors("empty")).toEqual(false)
+    expect(changeset.hasErrors("none")).toEqual(false)
+  })
+})
+
+describe("hasAnyErrors()", () => {
+  test("true when at least one error is set", () => {
+    const changeset = new Changeset({
+      errors: {
+        test: [{ message: "Error" }],
+      },
+    })
+
+    expect(changeset.hasAnyErrors()).toEqual(true)
+  })
+
+  test("false when no errors are set", () => {
+    const changeset = new Changeset({
+      errors: {
+        empty: [],
+      },
+    })
+
+    expect(changeset.hasAnyErrors()).toEqual(false)
+  })
+})
+
+describe("clearErrors()", () => {
+  test("clears errors for a given field", () => {
+    const changeset = new Changeset({
+      errors: {
+        test: [{ message: "Error" }],
+      },
+    })
+
+    changeset.clearErrors("test")
+    expect(changeset.getAllErrors()).toStrictEqual({})
+  })
+})
+
+describe("clearAllErrors()", () => {
+  test("clears all errors", () => {
+    const changeset = new Changeset({
+      errors: {
+        test: [{ message: "Error" }],
+        empty: [],
+      },
+    })
+
+    changeset.clearAllErrors()
+    expect(changeset.getAllErrors()).toStrictEqual({})
   })
 })
--- src/changeset.ts
+++ src/changeset.ts
@@ -7,11 +7,12 @@ export interface Changes {
 }
 
 export interface FieldError {
-  message: string
+  message: string | null | undefined
+  [key: string]: any
 }
 
 export interface AllFieldErrors {
-  [field: string]: FieldError[]
+  [field: string]: FieldError[] | null | undefined
 }
 
 export interface Options {
@@ -25,10 +26,21 @@ export class Changeset {
   private changes: Changes = {}
   private errors: AllFieldErrors = {}
 
-  constructor(options: Options) {
-    this.originals = options.originals || {}
-    this.changes = options.changes || {}
-    this.errors = options.errors || {}
+  constructor(options: Options = {}) {
+    if (options.originals) {
+      verifyOriginals(options.originals)
+      this.originals = options.originals
+    }
+
+    if (options.changes) {
+      verifyChanges(options.changes)
+      this.changes = options.changes
+    }
+
+    if (options.errors) {
+      verifyAllErrors(options.errors)
+      this.errors = options.errors
+    }
   }
 
   getValue(field: string): any {
@@ -37,6 +49,10 @@ export class Changeset {
     } else {
       return this.originals[field]
     }
+  }
+
+  getValues(): Originals {
+    return { ...this.originals, ...this.changes }
   }
 
   getOriginals(): Originals {
@@ -49,6 +65,11 @@ export class Changeset {
 
   setOriginal(field: string, value: any): void {
     this.originals[field] = value
+  }
+
+  setOriginals(originals: Originals): void {
+    verifyOriginals(originals)
+    this.originals = originals
   }
 
   hasOriginal(field: string): boolean {
@@ -67,12 +88,17 @@ export class Changeset {
     this.changes[field] = value
   }
 
-  clearChanges(): void {
-    this.changes = {}
+  setChanges(changes: Changes): void {
+    verifyChanges(changes)
+    this.changes = changes
   }
 
   hasChange(field: string): boolean {
     return this.changes.hasOwnProperty(field)
+  }
+
+  clearChanges(): void {
+    this.changes = {}
   }
 
   getAllErrors(): AllFieldErrors {
@@ -80,19 +106,32 @@ export class Changeset {
   }
 
   getErrors(field: string): FieldError[] {
-    return this.errors[field]
+    return this.errors[field] || []
   }
 
   setErrors(field: string, errors: FieldError[]): void {
+    verifyErrors(errors)
     this.errors[field] = errors
   }
 
   setAllErrors(errors: AllFieldErrors): void {
+    verifyAllErrors(errors)
     this.errors = errors
   }
 
+  addError(field: string, error: FieldError): void {
+    verifyError(error)
+
+    if (!Array.isArray(this.errors[field])) {
+      this.errors[field] = []
+    }
+
+    ;(this.errors[field] as any).push(error)
+  }
+
   hasErrors(field: string): boolean {
-    return Array.isArray(this.errors[field]) && this.errors[field].length > 0
+    const errors = this.errors[field]
+    return Array.isArray(errors) && errors.length > 0
   }
 
   hasAnyErrors(): boolean {
@@ -103,5 +142,73 @@ export class Changeset {
     }
 
     return false
+  }
+
+  clearErrors(field: string): void {
+    delete this.errors[field]
+  }
+
+  clearAllErrors(): void {
+    this.errors = {}
+  }
+}
+
+function verifyOriginals(originals: Originals): void {
+  verifyData("originals", originals)
+}
+
+function verifyChanges(changes: Changes): void {
+  verifyData("changes", changes)
+}
+
+function verifyData(dataType: string, data: Originals | Changes): void {
+  if (typeof data !== "object") {
+    throw new Error(`Changeset ${dataType} must be an object`)
+  } else if (data === null) {
+    throw new Error(`Changeset ${dataType} can't be null`)
+  } else if (data === undefined) {
+    throw new Error(`Changeset ${dataType} can't be undefined`)
+  } else if (Array.isArray(data)) {
+    throw new Error(`Changeset ${dataType} can't be an array`)
+  }
+}
+
+function verifyAllErrors(allErrors: AllFieldErrors): void {
+  if (typeof allErrors !== "object") {
+    throw new Error("Changeset errors must be an object")
+  } else if (Array.isArray(allErrors)) {
+    throw new Error("Changeset errors can't be an array")
+  } else {
+    for (const field of Object.keys(allErrors)) {
+      const errors = allErrors[field]
+
+      if (errors !== null && errors !== undefined) {
+        verifyErrors(errors)
+      }
+    }
+  }
+}
+
+function verifyErrors(errors: FieldError[]): void {
+  if (!Array.isArray(errors)) {
+    throw new Error("Changeset field errors must be an array")
+  } else {
+    errors.forEach(verifyError)
+  }
+}
+
+function verifyError(error: FieldError): void {
+  if (typeof error !== "object") {
+    throw new Error("Changeset error must be an object")
+  } else if (Array.isArray(error)) {
+    throw new Error("Changeset error can't be an array")
+  } else if (!error.hasOwnProperty("message")) {
+    throw new Error("Changeset error must have a message property")
+  } else if (
+    typeof error.message !== "string" &&
+    error.message !== null &&
+    error.message !== undefined
+  ) {
+    throw new Error("Changeset error message must be a string")
   }
 }
